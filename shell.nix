0d5228edff8978cb651a205023fffe257c57f55f
Artemis Tosini <me@artem.ist>
2021-05-05 01:07:36 +0000

    Place diodes

---
+++ shell.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> { };
+
+let
+  python = python3;
+  pythonPackages = python.pkgs;
+    toPythonModule = drv:
+      drv.overrideAttrs( oldAttrs: {
+        # Use passthru in order to prevent rebuilds when possible.
+        passthru = (oldAttrs.passthru or {})// {
+          pythonModule = python;
+          pythonPath = [ ]; # Deprecated, for compatibility.
+          requiredPythonModules = requiredPythonModules drv.propagatedBuildInputs;
+        };
+      });
+
+in
+mkShell rec {
+  name = "thing-env";
+  buildInputs = [
+    python3
+    python3Packages.ipython
+    (toPythonModule (pkgs.kicad-unstable.override { python3 = python3; }).src)
+  ];
+}
