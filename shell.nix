112dd9ad00c211b95bb6cb0b6f9fef08581d660d
Artemis Tosini <me@artem.ist>
2021-05-14 01:01:28 +0000

    Make LEDs make more sense, with automation!

--- shell.nix
+++ shell.nix
@@ -3,19 +3,19 @@ with import <nixpkgs> { };
 let
   python = python3;
   pythonPackages = python.pkgs;
-    toPythonModule = drv:
-      drv.overrideAttrs( oldAttrs: {
-        # Use passthru in order to prevent rebuilds when possible.
-        passthru = (oldAttrs.passthru or {})// {
-          pythonModule = python;
-          pythonPath = [ ]; # Deprecated, for compatibility.
-          requiredPythonModules = requiredPythonModules drv.propagatedBuildInputs;
-        };
-      });
+  toPythonModule = drv:
+    drv.overrideAttrs (oldAttrs: {
+      # Use passthru in order to prevent rebuilds when possible.
+      passthru = (oldAttrs.passthru or { }) // {
+        pythonModule = python;
+        pythonPath = [ ]; # Deprecated, for compatibility.
+        requiredPythonModules = requiredPythonModules drv.propagatedBuildInputs;
+      };
+    });
 
 in
 mkShell rec {
-  name = "thing-env";
+  name = "kicad-env";
   buildInputs = [
     python3
     python3Packages.ipython
